43df8dff04fc4f44983c375bd0ab59623e9f0d9f
raghda244 <raghdamohsen244@gmail.com>
2021-05-11 15:54:02 +0200

    updated project

--- app.js
+++
@@ -1,9 +0,0 @@
-import {category} from './enums.js';
-//var account=require('category');
-//const account2=require("./chat");
-var enums =category.t1;
-//var acc=new account();
-//let s= account.t1;
-console.log(enums);
-console.log('dfgh');
-//acc.print_hhh();
