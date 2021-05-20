73fdf41010a7b6b18e8d29889bd7801e55c8caa9
raghda244 <raghdamohsen244@gmail.com>
2021-05-20 15:29:58 +0200

    Update user.js

--- Routes/user.js
+++ Routes/user.js
@@ -2,6 +2,6 @@ const bcrypt=require('bcrypt');
 var express = require('express');
 var router = express.Router();
 const userCotnroller=require('../Controllers/UserController');
-router.get('/UserLogin',userCotnroller.getUser);
+router.get('/UserLogin',userCotnroller.GetUser);
 
 module.exports=router;
